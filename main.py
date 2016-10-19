bbbea1520e60b5046c464ede469efd5dbdadda15
Rafael Milleo Carrenho <rafael.carrenho@hdi.com.br>
2016-10-19 13:14:16 -0200

    Removendo arquivos antigos e adicionando menu de opcoes

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+# -*- coding: utf-8 -*-
 import copy
-import random
-import os
-import time
 import heapq
+import os
+import random
+import time
 
-class Puzzle_Board():
-	PUZZLE_SOLUTION = [['1','2','3'],['4','5','6'],['7','8',' ']]
-	pieces = copy.deepcopy(PUZZLE_SOLUTION)
-	emptySpaceRow = 2
-	emptySpaceColumn = 2
-	lastMove = ''
-	moves = 0
+class Puzzle_Board(object):
 
-	def __init__(self, pieces_set = ''):
+	def __init__(self, pieces_set = '', moves = 0):
+		self.PUZZLE_SOLUTION = [['1','2','3'],['4','5','6'],['7','8',' ']]
+		self.pieces = copy.deepcopy(self.PUZZLE_SOLUTION)
+		self.emptySpaceRow = 2
+		self.emptySpaceColumn = 2
+		self.lastMove = ''
+		self.moves = moves
+
 		if(pieces_set != ''):
 			self.pieces = pieces_set
 			self.emptySpaceRow, self.emptySpaceColumn = self.get_empty_piece_pos()
@@ -27,9 +29,6 @@ class Puzzle_Board():
 			output += row_str + "\n"
 
 		return output
-
-	def __hash__(self):
-		return hash(str(self.pieces))
 
 	def move_up(self):
 		if(self.emptySpaceRow > 0):
@@ -86,8 +85,19 @@ class Puzzle_Board():
 
 		return index
 
-	def get_score(self):
+	def is_solved(self):
 		if(self.pieces == self.PUZZLE_SOLUTION):
+			return True
+		return False
+
+class State(Puzzle_Board):
+
+	def __init__(self, pieces_set = '', moves = 0, parent = None):
+		super(State, self).__init__(pieces_set, moves)
+		self.parent = parent
+
+	def get_score(self):
+		if(self == self.PUZZLE_SOLUTION):
 			return 0
 
 		incorrect_pieces = 0
@@ -99,7 +109,7 @@ class Puzzle_Board():
 					piece_distances += self.get_heuristic_distance((row_i, piece_i), (g_row, g_column))
 					incorrect_pieces += 1
 
-		return incorrect_pieces + piece_distances + self.moves
+		return piece_distances + self.moves
 
 	def get_piece_correct_position(self, piece_value = ''):
 		for row_i, row in enumerate(self.PUZZLE_SOLUTION):
@@ -112,6 +122,8 @@ class Puzzle_Board():
 	def get_heuristic_distance(self, piece_coord, goal_coord):
 		return (abs(piece_coord[0] - goal_coord[0]) + (abs(piece_coord[1] - goal_coord[1])))
 
+	def __hash__(self):
+		return hash(str(self.pieces))
 
 	def __lt__(self, other):
 		return self.get_score() < other.get_score()
@@ -122,71 +134,6 @@ class Puzzle_Board():
 	def __eq__(self, other):
 		return self.__cmp__(other)
 
-class SmartBoy():
-	history = set()
-
-	def solveIt(self, verbose = True):
-		puzzle = Puzzle_Board()
-		puzzle.randomize()
-		openset = PriorityQueue()
-		openset.add(puzzle)
-
-		if(verbose == True):
-			print(puzzle)
-
-		while openset:
-			current = openset.poll()
-
-			if(self.is_solved(current)):
-				puzzle = current
-				break
-
-			possibilities = self.get_possible_moves(current)
-
-			for possible_move, puzzle in possibilities.iteritems():
-				if hash(puzzle) not in self.history:
-					openset.add(puzzle)
-
-			self.history.add(hash(current))
-			puzzle.moves = puzzle.moves + 1
-
-
-			if(verbose == True):
-				time.sleep(0.1)
-				print(chr(27) + "[2J")
-				print(puzzle.moves)
-				print(current)
-
-			
-
-		if(verbose == True):
-			print("DONE IN " + str(puzzle.moves) + " MOVES")
-
-	def is_solved(self, puzzle):
-		if(puzzle == puzzle.PUZZLE_SOLUTION):
-			return True
-		return False
-
-	def get_possible_moves(self, puzzle):
-		if(puzzle == puzzle.PUZZLE_SOLUTION):
-			return False
-
-		possibilities = {}
-		moves = ['up', 'down', 'left', 'right']
-
-		for move in moves:
-			aux = Puzzle_Board(copy.deepcopy(puzzle.pieces))
-			aux.moves = puzzle.moves
-			possible = getattr(aux, 'move_%s' % move)()
-
-			if (possible == True):
-				possibilities[move] = aux
-			aux = None
-
-		return possibilities
-
-	
-		
 class PriorityQueue:
 	pq = []
 
@@ -204,10 +151,123 @@ class PriorityQueue:
 	def __len__(self):
 		return len(self.pq)
 
-iterations_avg = 0
-sb = SmartBoy()
-sb.solveIt()
-#for i in range(0, 100):
-#	iterations_avg += sb.solveIt(False)
-#
-#print(iterations_avg / 100)
+class SmartBoy():
+
+	def solveIt(self, verbose = False, passo_a_passo = False):
+		start = time.time()
+		self.history = set()
+		puzzle = Puzzle_Board()
+		openset = PriorityQueue()
+		puzzle.randomize()
+
+		if(verbose == True):
+			print("Estado inicial do quebra-cabeças")
+			print(puzzle)
+
+		start_state = State(puzzle.pieces)
+		openset.add(start_state)
+
+		while openset:
+			current = openset.poll()
+
+			if(current == puzzle.PUZZLE_SOLUTION):
+				end = time.time()
+				
+				if(verbose == True):
+					path = self.rebuild_path(current)
+					for state in reversed(path):
+						print(state)
+						if(passo_a_passo == True):
+							raw_input()
+
+					print("=" * 30)
+					print("Quebra-cabeças solucionado em " + str(current.moves) + " movimentos")
+					print("Em um total de: " + str(float(end - start)) + " segundos")
+					print("=" * 30 + "\n\n")
+				
+				return current.moves, float(end - start)
+
+			possibilities = self.get_possible_moves(current)
+			for move, state in possibilities.iteritems():
+				if hash(state) not in self.history:
+					openset.add(state)
+
+			self.history.add(hash(current))
+
+	def get_possible_moves(self, puzzle):
+		possibilities = {}
+		moves = ['up', 'down', 'left', 'right']
+
+		moves_count = puzzle.moves + 1
+		for move in moves:
+			state = State(copy.deepcopy(puzzle.pieces), moves_count, puzzle)
+			possible = getattr(state, 'move_%s' % move)()
+
+			if (possible == True):
+				possibilities[move] = state
+			state = None
+
+		return possibilities
+
+	def rebuild_path(self, end):
+		path = [end]
+		state = end.parent
+		while state.parent:
+			path.append(state)
+			state = state.parent
+
+		return path
+
+
+def sair():
+	exit()
+
+def resolver_quebra_cabecas():
+	sm = SmartBoy()
+	sm.solveIt(True)
+
+def media_resolucao():
+	qtde_iteracoes = raw_input("Insira a quantidade de iterações para extrair a média [Padrão: 100]: ")
+	total_movimentos = 0
+	total_tempo = 0.0
+
+	for i in range(1, int(qtde_iteracoes)):
+		sm = SmartBoy()
+		movimentos, tempo = sm.solveIt()
+		total_movimentos += movimentos
+		total_tempo += tempo
+
+	media_movimentos = total_movimentos / int(qtde_iteracoes)
+	media_tempo = total_tempo / int(qtde_iteracoes)
+
+	print("Média de movimentos para cada solução: " + str(media_movimentos))
+	print("Tempo médio para cada solulção: " + str(media_tempo) + " segundos")
+
+def passo_a_passo():
+	sm = SmartBoy()
+	sm.solveIt(True, True)
+
+while(True):
+	opcoes = {
+		1: { 'label': "Solucionar Quebra-cabeças", 'function': resolver_quebra_cabecas },
+		2: { 'label': "Ver o passo-a-passo da solução do quebra-cabeças", 'function': passo_a_passo },
+		3: { 'label': "Extrair média de resolução por movimentos e tempo", 'function': media_resolucao },
+		4: { 'label': "Sair", 'function': sair }
+	}
+
+	os.system('clear')
+
+	print("=" * 30)
+	print(("=" * 10) + " 8 Puzzle " + ("=" * 10))
+	print("=" * 30)
+	for numero, opcao in opcoes.iteritems():
+		print(str(numero) + ' - ' + opcao['label'])
+
+	opcao_input = raw_input("\nOpcao: ")
+
+	opcao_escolhida = opcoes.get(int(opcao_input), lambda: "nothing")
+	funcao = opcao_escolhida['function']
+
+	funcao()
+
+	raw_input("Pressione qualquer tecla para continuar...")
